acfd2ade4d060db0e764853a33e4ab1eef8e42b7
AndersenSemenov <andersen5447@gmail.com>
2022-06-06 01:03:48 +0300

    Deleted unnecessary files

--- server/cmake-build-debug/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /utbot_distr/gtest/googletest
-# Build directory: /home/utbot/UTBotCpp/server/cmake-build-debug/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
